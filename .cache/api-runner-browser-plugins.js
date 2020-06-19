9e17661bb6466f325e123e92c122922c96ed9a17
QuintonJason <quinton.jason@gmail.com>
2020-06-19 14:00:47 -0500

    initial eleventy commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#031C2D","theme_color":"#031C2D","display":"minimal-ui","icon":"src/images/activate-rocket.png"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
